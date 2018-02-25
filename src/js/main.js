d4820bcce3e4f80178cabc0b71e6ea133b9951bc
Itadakimas <said.ahemt@gmail.com>
2018-02-25 23:37:14 +0100

    adds gooey effect without neon light

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,22 @@
 import 'core/polyfills';
 import SVG4Everybody from 'svg4everybody';
+import { TweenMax, Linear } from "gsap";
+
+
+function animateLoader()
+{
+  TweenMax.to(document.querySelector("#disc"), 5, {
+    ease: Linear.easeNone,
+    repeat: -1,
+    rotation: "-=360",
+    transformOrigin: "50% 50%"
+  });
+}
+
 
 SVG4Everybody();
 document.addEventListener('DOMContentLoaded', function() {
 
+  animateLoader();
   console.log('main.js loaded');
 });
